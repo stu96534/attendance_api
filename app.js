264959f6732ff2ce3824e3d0efef3cecceba7b01
stu96534 <stu96534@gmail.com>
2022-12-20 22:17:19 +0800

    feat: add cors

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require('express')
 const passport = require('./config/passport')
+const cors = require('cors')
 
 const apis = require('./routes/apis')
 
 const app = express()
 
+const corsOptions = {
+  origin: [
+    'http://127.0.0.1:5173/'
+  ],
+  methods: [
+    'GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'PATCH', 'OPTIONS'
+  ],
+  allowedHeaders: ['content-type', 'authorization']
+}
+
 const port = process.env.PORT || 3000
 
+app.use(cors(corsOptions))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
