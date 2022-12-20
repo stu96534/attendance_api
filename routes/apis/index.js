f81cd687f3b7f8f0405640cd9c6812881d0bf432
stu96534 <stu96534@gmail.com>
2022-12-20 17:26:42 +0800

    test: attendant router remove authenticate

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -5,15 +5,15 @@ const passport = require('../../config/passport')
 const attendant = require('./modules/attendant')
 const userController = require('../../controllers/user-controller')
 const { authenticated } = require('../../middleware/auth')
-const { authErrorHandler } = require('../../middleware/error-handler')
+const { authErrorHandler, apiErrorHandler } = require('../../middleware/error-handler')
 
 
 router.post('/users/signin', passport.authenticate('local', { session: false, failWithError: true }), userController.signIn, authErrorHandler)
 
-router.use('/attendant', authenticated, attendant)
+router.use('/attendant', attendant)
 
 router.use('/', (req, res) => res.redirect('/api/attendant'))
 
-router.use('/', (req, res) => res.redirect('/api/attendant'))
+router.use('/', apiErrorHandler )
 
 module.exports = router
