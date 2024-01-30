a99c47384f00ab48a90226ba6572d3ac0c86ac8b
stu96534 <stu96534@gmail.com>
2024-01-30 20:06:24 +0800

    feat: add connection pool

--- middleware/funcTools.js
+++ middleware/funcTools.js
@@ -17,35 +17,8 @@ const isNotPair = (value1, value2) => {
   if (value1 !== value2) return true
 }
 
-const getCalendarId = async (dateValue) => {
-  
-  const year = new Date(dateValue).getFullYear().toString()
-  const CalendarModel = Calendars[year].model
-
-  const CalendarDate = await CalendarModel.findOne({
-    where: {
-      date: GMT_3(dateValue)
-    },
-    raw: true,
-  })
-
-  return CalendarDate.id
-}
-
-const getUserId = async (userName) => {
-  const user = await User.findOne({
-    where: {
-      account: userName
-    },
-    raw: true
-  })
-  return user.id
-}
-
 
 module.exports = {
   isNotPair,
-  getCalendarId,
-  getUserId,
   Calendars
 }
